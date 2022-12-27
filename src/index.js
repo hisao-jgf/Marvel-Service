6ddebd779caf73e7e48677dd2d493244e81c75b6
hisaojgf <hisaojgf@gmail.com>
2022-12-27 23:16:03 +0300

    Updated imports and core structure

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './components/app/App';
+
 import './style/style.scss';
 
-ReactDOM.render(
-  <React.StrictMode>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
 );
 
