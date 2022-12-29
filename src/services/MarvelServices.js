a831335f3526dd67993cd17034f01241a779f173
hisaojgf <hisaojgf@gmail.com>
2022-12-29 21:54:31 +0300

    Optimized data load and structuring

--- src/services/MarvelServices.js
+++ src/services/MarvelServices.js
@@ -9,12 +9,23 @@ class MarvelServices {
         }
         return await getResult.json();
     };
+    _transformCharacterData = (char) => {
+        return {
+            name: char.name,
+            description: char.description,
+            thumbnail: `${char.thumbnail.path}.${char.thumbnail.extension}`,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url
+        }
+    }
 
     getAllCharacters = async () => {
-        return await this.getResources(`${this._apiBase}characters?limit=9&offset=210&apikey=${this._apiKey}`)
+        const result = await this.getResources(`${this._apiBase}characters?limit=9&offset=210&apikey=${this._apiKey}`);
+        return result.data.results.map(this._transformCharacterData);
     }
     getCharacter = async (id) => {
-        return await this.getResources(`${this._apiBase}characters/${id}?apikey=${this._apiKey}`);
+        const result = await this.getResources(`${this._apiBase}characters/${id}?apikey=${this._apiKey}`);
+        return this._transformCharacterData(result.data.results[0]);
     }
 }
 
