7e0cc851dd61b6f0e32a5b99b314d48f07898a79
hisaojgf <hisaojgf@gmail.com>
2022-12-29 23:02:06 +0200

    Fixed character image 'Not Found' display

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -31,11 +31,15 @@ class RandomChar extends Component {
 
     render() {
         const { name, description, thumbnail, homepage, wiki } = this.state;
+        let imgStyle = {'objectFit': 'cover'};
+        if (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+            imgStyle = {'objectFit': 'unset'};
+        }
         
         return (
             <div className="randomchar">
                 <div className="randomchar__block">
-                    <img src={thumbnail} alt="Random character" className="randomchar__img"/>
+                    <img src={thumbnail} alt="Random character" className="randomchar__img" style={imgStyle}/>
                     <div className="randomchar__info">
                         <p className="randomchar__name">{name}</p>
                         <p className="randomchar__descr">
