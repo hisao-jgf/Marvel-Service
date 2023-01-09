038ec7db9f2f824a5adcf77143511222c3978ba8
hisaojgf <hisaojgf@gmail.com>
2023-01-09 23:57:04 +0100

    Added data load and error handling on request, wrapped re-usable functions in useCallback hook

--- src/hooks/request.hook.js
+++ src/hooks/request.hook.js
@@ -1,17 +1,33 @@
-export const useRequest = () => {
+import { useState, useCallback } from "react";
 
-    const request = async (url, method = 'GET', body = null, headers = {'Content-type': 'application/json'}) => {
-        const response = await fetch(url, {method, body, headers});
+const useRequest = () => {
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
 
-        if (!response.ok) {
-            throw new Error(`Could not find ${url}, status: ${response.status}`);
+    const request = useCallback(async (url, method = 'GET', body = null, headers = {'Content-type': 'application/json'}) => {
+        setLoading(true);
+
+        try {
+            const response = await fetch(url, {method, body, headers});
+
+            if (!response.ok) {
+                throw new Error(`Could not find ${url}, status: ${response.status}`);
+            }
+
+            const data = await response.json();
+
+            setLoading(false);
+            return data;
+        } catch(e) {
+            setLoading(false);
+            setError(e.message);
+            throw e;
         }
+    }, []);
 
-        const data = await response.json();
-        return data;
-    }
+    const removeError = useCallback(() => setError(null), []);
 
-    return {request};
+    return {loading, error, removeError, request};
 }
 
 export default useRequest;
