98520e120ce12f103cd01e4541343381b3b7122c
hisaojgf <hisaojgf@gmail.com>
2023-01-10 16:39:02 +0100

    Added comics data request and transformation methods

--- src/services/MarvelServices.js
+++ src/services/MarvelServices.js
@@ -6,6 +6,7 @@ const useMarvelServices = () => {
     const _apiKey = process.env.REACT_APP_API_KEY;
     const _apiBase = 'https://gateway.marvel.com:443/v1/public/';
     const _baseCharacterOffset = 210;
+    const _baseComicsOffset = 0;
 
     const _transformCharacterData = (char) => {
         return {
@@ -18,6 +19,17 @@ const useMarvelServices = () => {
             comics: char.comics.items
         }
     }
+    const _transformComicsData = (comic) => {
+        return {
+            id: comic.id,
+            title: comic.title,
+            description: comic.description || 'There is no description available for this comic.',
+            thumbnail: `${comic.thumbnail.path}.${comic.thumbnail.extension}`,
+            pages: comic.pageCount ? `${comic.pageCount} p.` : 'There is no information available about the number of pages.',
+            language: comic.textObjects.language || 'en-us',
+            price: comic.prices[0].price ? `${comic.prices[0].price}$` : 'Not available'
+        }
+    }
 
     const getAllCharacters = async (offset = _baseCharacterOffset) => {
         const result = await request(`${_apiBase}characters?limit=9&offset=${offset}&apikey=${_apiKey}`);
@@ -28,7 +40,16 @@ const useMarvelServices = () => {
         return _transformCharacterData(result.data.results[0]);
     }
 
-    return {loading, error, removeError, request, getCharacter, getAllCharacters};
+    const getAllComics = async (offset = _baseComicsOffset) => {
+        const result = await request(`${_apiBase}comics?limit=8&offset=${offset}&apikey=${_apiKey}`);
+        return result.data.results.map(_transformComicsData);
+    }
+    const getComic = async (id) => {
+        const result = await request(`${_apiBase}comics/${id}?apikey=${_apiKey}`)
+        return _transformComicsData(result.data.results[0]);
+    }
+
+    return {loading, error, removeError, request, getCharacter, getAllCharacters, getComic, getAllComics};
 }
 
 export default useMarvelServices;
