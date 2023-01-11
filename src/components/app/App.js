d36a498a28d4eef83ecd120fdc8eb060a5e14f41
hisaojgf <hisaojgf@gmail.com>
2023-01-11 19:24:58 +0100

    Added pages routing core functionality

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,14 @@
 import { useState } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import ErrorBoundary from '../errorBoundary/ErrorBoundary';
 
 import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
+
+import AppBanner from '../appBanner/AppBanner';
+import ComicsList from '../comicsList/ComicsList';
 
 import decoration from '../../resources/img/vision.png';
 
@@ -16,24 +20,35 @@ const App = () => {
     }
 
     return (
-        <div className="app">
+        <Router>
+            <div className="app">
             <AppHeader/>
-            <main>
-                <ErrorBoundary>
-                    <RandomChar/>
-                </ErrorBoundary>
-                <div className="char__content">
-                    <ErrorBoundary>
-                        <CharList onCharacterSelected={onCharacterSelected}/>
-                    </ErrorBoundary>
-                    
-                    <ErrorBoundary>
-                        <CharInfo characterId={selectedCharacter}/>
-                    </ErrorBoundary>
-                </div>
-                <img className="bg-decoration" src={decoration} alt="vision"/>
-            </main>
-        </div>
+                <main>
+                    <Switch>
+                        <Route exact path="/">
+                            <ErrorBoundary>
+                                <RandomChar />
+                            </ErrorBoundary>
+                            <div className="char__content">
+                                <ErrorBoundary>
+                                    <CharList onCharacterSelected={onCharacterSelected} />
+                                </ErrorBoundary>
+                                
+                                <ErrorBoundary>
+                                    <CharInfo characterId={selectedCharacter} />
+                                </ErrorBoundary>
+                            </div>
+                            <img className="bg-decoration" src={decoration} alt="vision"/>
+                        </Route>
+
+                        <Route exact path="/comics">
+                            <AppBanner />
+                            <ComicsList />
+                        </Route>
+                    </Switch>
+                </main>
+            </div>
+        </Router>
     )
 }
 
--- src/components/appHeader/AppHeader.js
+++ src/components/appHeader/AppHeader.js
@@ -1,18 +1,39 @@
+import { NavLink } from 'react-router-dom';
 import './appHeader.scss';
 
 const AppHeader = () => {
     return (
         <header className="app__header">
             <h1 className="app__title">
-                <a href="#">
+                <NavLink to="/">
                     <span>Marvel</span> information portal
-                </a>
+                </NavLink>
             </h1>
             <nav className="app__menu">
                 <ul>
-                    <li><a href="#">Characters</a></li>
+                    <li>
+                        <NavLink 
+                            to="/"
+                            exact
+                            activeStyle={{
+                                color: '#9F0013'
+                            }}
+                        >
+                            Characters
+                        </NavLink>
+                    </li>
                     /
-                    <li><a href="#">Comics</a></li>
+                    <li>
+                        <NavLink
+                            exact
+                            to="/comics"
+                            activeStyle={{
+                                color: '#9F0013'
+                            }}
+                        >
+                            Comics
+                        </NavLink>
+                    </li>
                 </ul>
             </nav>
         </header>
